a1c9f005b5a1e49c5053c2364c3f306022bc34b4
Edin Gogic <B00106393@mytudublin.ie>
2020-03-30 16:59:07 +0100

    resolved issue with totalItems was missing $

--- resources/views/products/displaygrid.blade.php
+++ resources/views/products/displaygrid.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 @section('content')
-@include('flash::message') 
-@php $j=0 @endphp 
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script> 
 <div style="padding-top:7%" class="container-fluid"> 
     <nav class="navbar navbar-default navbar-fixed-top"> 
@@ -9,12 +7,14 @@
             <li><button id="checkOut" onclick="window.location.href=''" type="button" class="btn btn-primary navbar-btn center-block">Check Out</button></a></li> 
             <li><button id="emptycart" type="button" class="btn btn-primary navbar-btn center-block">Empty Cart</button></li> 
             <li><span style="font-size:40px;margin-right:0px;" class="glyphicon glyphicon-shopping-cart navbar-btn"></span></li> 
-            <li><div class="navbar-text" id="shoppingcart" style="font-size:12pt;margin-left:0px;margin-right:0px;">{{totalItems}}</div></li> 
+            <li><div class="navbar-text" id="shoppingcart" style="font-size:12pt;margin-left:0px;margin-right:0px;">{{$totalItems}}</div></li> 
             <li><div class="navbar-text" style="font-size:14pt;margin-left:0px;">Item(s)</div></li> 
         <ul> 
     </nav> 
 </div>
 
+@include('flash::message') 
+@php $j=0 @endphp 
 @foreach($products as $product) 
     @if ($j==0) <div class='row'> @endif 
         <div class="col-sm-4"> 
